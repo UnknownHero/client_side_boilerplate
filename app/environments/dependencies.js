be36558b866f1001bbeca06a93d50bdbc8c3dea8
UnknownHero <unknownpostie@gmail.com>
2013-09-11 02:45:58 +0400

    refactoring

---
+++ app/environments/dependencies.js
@@ -0,0 +1,35 @@
+define(['.', 'app' , 'underscore','routers/main', 'routers/denied'],
+    function (Ember, App, _, routers_main, routers_denied) {
+
+        var locator = App.get("locator"),
+            injections = {},
+            envInjections = {},
+            env = "default",
+            globalModule = window;
+
+        env = (globalModule.AppEnv) ? globalModule.AppEnv : env;
+
+        switch (env) {
+            case "ie":
+                envInjections = {
+                    baseRouter: routers_denied
+                }
+                break;
+
+            default:
+                envInjections = {
+                    baseRouter: routers_main
+                }
+                break;
+        }
+
+        _.extend(injections, envInjections);
+
+
+        for (injectionIndex in injections) {
+            locator.set(injectionIndex, injections[injectionIndex]);
+        }
+
+        return locator;
+
+    });
